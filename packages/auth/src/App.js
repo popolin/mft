58df194fd7438ddb86f51a20c25f11f243311929
popolin <mulequedoido@gmail.com>
2022-06-08 18:18:21 -0300

    Auth

---
+++ packages/auth/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Switch, Route, Router } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+import Signup from './components/Signup';
+import Signin from "./components/Signin";
+import SignIn from "./components/Signin";
+import SignUp from "./components/Signup";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'au'
+});
+
+export default ({ history, onSignIn }) => {
+  return (
+    <div>
+        <Router history={history}>
+          <StylesProvider generateClassName={ generateClassName }>
+            <Switch>
+              <Route path="/auth/signin">
+                <SignIn onSignIn={onSignIn}/>
+              </Route>
+              <Route path="/auth/signup">
+                <SignUp onSignIn={onSignIn}/>
+              </Route>
+            </Switch>
+          </StylesProvider>
+        </Router>
+    </div>
+  )
+}
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
-const mount = (el) => {
+const mount = (el, { onNavigate, defaultHistory, initialPath } = {}) => {
+  const history = defaultHistory || createMemoryHistory({
+    initialEntries: [initialPath]
+  });
+  if(onNavigate){
+    history.listen(onNavigate);
+  }
+
   ReactDOM.render(
-    <App />,
+    <App history={history} />,
     el
   )
+
+  return {
+    onParentNavigate({ pathname: nextPathName }) {
+      const { pathname } = history.location;
+      if(pathname !== nextPathName){
+        history.push(nextPathName);
+      }
+    }
+  }
 }
 
 if(process.env.NODE_ENV === 'development'){
   const devRoot = document.querySelector('#_marketing-dev-root');
   if(devRoot){
-   mount(devRoot);
+   mount(devRoot, {
+     defaultHistory: createBrowserHistory()
+   });
   }
 }
 
