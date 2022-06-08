94991a346867f772a07ccd99f713437ed601a57a
popolin <mulequedoido@gmail.com>
2022-06-08 18:51:37 -0300

    Atf

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -24,8 +24,6 @@ const mount = (el, { onNavigate, defaultHistory, onSignIn }) => {
   };
 };
 
-// If we are in development and in isolation,
-// call mount immediately
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_auth-dev-root');
 
